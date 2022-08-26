b0788420cc8f072a725b5339415a36f0d1c87e5c
Radical7VII <2246135252@qq.com>
2022-08-26 22:32:58 +0800

    开学上传

--- behavior_pack_qIpzvHge/CodingTest/uiScript/control_panel.py
+++ behavior_pack_qIpzvHge/CodingTest/uiScript/control_panel.py
@@ -10,8 +10,12 @@ class control_panel(ScreenNode):
 		ScreenNode.__init__(self, namespace, name, param)
 		self.aim_path = '/aim'
 		self.control_path = '/control'
+		self.progress_path = '/progress_bar'
+		progress_path = '/progress_bar'
 		self.client_system = clientApi.GetSystem('CodingTest', 'CodingClient')
 		self.playerId = clientApi.GetLocalPlayerId()
+		self.bullet_number = 0
+		progress_path.GetBaseUIControl
 
 	def Create(self):
 		"""
@@ -21,11 +25,14 @@ class control_panel(ScreenNode):
 		# 按钮注册
 		self.shoot_button = self.control_item.GetChildByPath('/shoot').asButton()
 		self.squid_button = self.control_item.GetChildByPath('/squid').asButton()
+		self.bullet_count = self.GetBaseUIControl(self.progress_path).asProgressBar('/filled_progress_bar')
 		# 按钮回调
 		self.shoot_button.AddTouchEventParams({'isSwallow': False})
 		self.shoot_button.SetButtonTouchUpCallback(self.shoot_button_callback)
 		self.squid_button.AddTouchEventParams({'isSwallow': False})
 		self.squid_button.SetButtonTouchUpCallback(self.squid_button_callback)
+		# 进度条
+		self.bullet_count.SetValue(self.bullet_number)
 
 	def shoot_button_callback(self, data):
 		"""射击按钮按下"""
@@ -34,6 +41,8 @@ class control_panel(ScreenNode):
 
 	def squid_button_callback(self, data):
 		print 'squid button callback'
+		self.bullet_number += 0.1
+		self.bullet_count.SetValue(self.bullet_number)
 
 	def Destroy(self):
 		"""
